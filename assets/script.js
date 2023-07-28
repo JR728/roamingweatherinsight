46c71101731f156da7f36ab2cc50fa32e88d1a78
JR728 <135184435+JR728@users.noreply.github.com>
2023-07-27 22:48:53 -0500

    added current weather

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const apiKey = "c0e4d96ff5bf292389be86ec1814fea3";
 const searchButton = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
+const currentWeatherDiv = document.getElementById("current-weather");
 
 searchButton.addEventListener("click", function () {
     const city = cityInput.value.trim();
@@ -14,9 +15,32 @@ async function getWeatherData(city) {
 
     try {
         const response = await fetch(apiUrl);
-        const weather = await response.json();
-        console.log(weather);
+        const weatherData = await response.json();
+        const currentWeather = weatherData.list[0];
+        
+        displayCurrentWeather(currentWeather, weatherData.city.name);
+        console.log(weatherData);
     } catch (error) {
         console.error("Error fetching weather data:", error);
     }
+
 }
+function displayCurrentWeather(currentWeather, cityName) {
+    const date = currentWeather.dt_txt;
+    const iconUrl = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
+    const temperature = currentWeather.main.temp;
+    const humidity = currentWeather.main.humidity;
+    const windSpeed = currentWeather.wind.speed;
+
+    const currentWeatherHTML = `
+        <h2>${cityName} - Current Weather</h2>
+        <p>Date: ${date}</p>
+        <img src="${iconUrl}" alt="Weather Icon">
+        <p>Temperature: ${temperature} °F</p>
+        <p>Humidity: ${humidity} %</p>
+        <p>Wind Speed: ${windSpeed} mph</p>
+    `;
+
+    currentWeatherDiv.innerHTML = currentWeatherHTML;
+}
+
