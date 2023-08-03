378affaf2cd30fe24baed0d4e4850df5f26b2920
JR728 <135184435+JR728@users.noreply.github.com>
2023-08-02 19:52:05 -0500

    added the 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ const apiKey = "c0e4d96ff5bf292389be86ec1814fea3";
 const searchButton = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
 const currentWeatherDiv = document.getElementById("current-weather");
+const forecastDiv = document.getElementById("forecast");
 
 searchButton.addEventListener("click", function () {
     const city = cityInput.value.trim();
@@ -11,19 +12,25 @@ searchButton.addEventListener("click", function () {
 });
 
 async function getWeatherData(city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
     try {
         const response = await fetch(apiUrl);
         const weatherData = await response.json();
         const currentWeather = weatherData.list[0];
-        
+        const forecast = getDailyForecast(weatherData.list);
+
         displayCurrentWeather(currentWeather, weatherData.city.name);
+        displayForecast(forecast);
         console.log(weatherData);
     } catch (error) {
         console.error("Error fetching weather data:", error);
     }
 
+}
+function getDailyForecast(forecastData) {
+
+    return forecastData.filter(day => day.dt_txt.includes("00:00:00"));
 }
 function displayCurrentWeather(currentWeather, cityName) {
     const date = currentWeather.dt_txt;
@@ -43,4 +50,25 @@ function displayCurrentWeather(currentWeather, cityName) {
 
     currentWeatherDiv.innerHTML = currentWeatherHTML;
 }
+function displayForecast(forecastData) {
+    let forecastHTML = "<h2>5-Day Forecast</h2>";
+    
+    forecastData.forEach(day => {
+        const date = new Date(day.dt_txt);
+        const iconUrl = `https://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+        const temperature = day.main.temp;
+        const humidity = day.main.humidity;
+        const windSpeed = day.wind.speed;
+        forecastHTML += `
+        <div class="forecast-day">
+            <p>Date: ${date.toDateString()}</p>
+            <img src="${iconUrl}" alt="Weather Icon">
+            <p>Temperature: ${temperature} °F</p>
+            <p>Humidity: ${humidity} %</p>
+            <p>Wind Speed: ${windSpeed} mph</p>
+        </div>
+    `;
+});
 
+forecastDiv.innerHTML = forecastHTML;
+}
